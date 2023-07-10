98edfc7b90a74b651ecf690171645edeab083828
nastyafureeva <AnaDFureeva@stud.kpfu.ru>
2023-07-10 18:40:31 +0300

    added calendar cell and controller

---
+++ MyHelper/MyHelper/Modules/Planer/CalendarCell.swift
@@ -0,0 +1,11 @@
+//
+//  PlanerCell.swift
+//  MyHelper
+//
+//  Created by Анастасия Фуреева on 07.07.2023.
+//
+
+import UIKit
+class CalendarCell: UICollectionViewCell{
+    @IBOutlet weak var dayOfMonth: UILabel!
+}
